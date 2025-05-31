06c6cfb1bfb920329ccc8a6beca9f4777bd39097
DavideSchiavone1997 <1997davide97@gmail.com>
2025-05-31 14:40:25 +0200

    Add files via upload

---
+++ HP.js
@@ -0,0 +1,205 @@
+
+const title = document.getElementById('title')
+
+title.addEventListener('mouseover', () => {
+
+
+    title.style.color = 'white';
+    title.style.fontSize = '30px';
+});
+
+title.addEventListener('mouseout', () => {
+
+    title.style.color = '#E1DA57';
+    title.style.fontSize = '25px';
+
+});
+
+////
+
+const leOrigini = document.getElementById('leOrigini')
+
+leOrigini.addEventListener('mouseover', () => {
+    leOrigini.style.color = 'white';
+    leOrigini.style.fontSize = '30px';
+
+})
+
+leOrigini.addEventListener('mouseout', () => {
+    leOrigini.style.color = '#E1DA57';
+    leOrigini.style.fontSize = '25px';
+
+})
+
+////
+
+const latrama = document.getElementById('trama')
+
+latrama.addEventListener('mouseover', () => {
+    latrama.style.color = 'white';
+    latrama.style.fontSize = '30px';
+
+})
+
+latrama.addEventListener('mouseout', () => {
+    latrama.style.color = '#E1DA57';
+    latrama.style.fontSize = '25px';
+
+})
+
+////
+
+const ispirazione = document.getElementById('ispirazione')
+ispirazione.addEventListener('mouseover', () => {
+    ispirazione.style.color = 'white';
+    ispirazione.style.fontSize = '30px';
+})
+
+ispirazione.addEventListener('mouseout', () => {
+    ispirazione.style.color = '#E1DA57';
+    ispirazione.style.fontSize = '25px';
+})
+
+///
+
+const Capitoli = document.getElementById('Capitoli')
+Capitoli.addEventListener('mouseover', () => {
+    Capitoli.style.color = 'white';
+    Capitoli.style.fontSize = '30px';
+})
+
+Capitoli.addEventListener('mouseout', () => {
+    Capitoli.style.color = '#E1DA57';
+    Capitoli.style.fontSize = '25px';
+})
+
+///
+
+const Personaggi = document.getElementById('Personaggi')
+Personaggi.addEventListener('mouseover', () => {
+    Personaggi.style.color = 'white';
+    Personaggi.style.fontSize = '30px';
+})
+
+Personaggi.addEventListener('mouseout', () => {
+    Personaggi.style.color = '#E1DA57';
+    Personaggi.style.fontSize = '25px';
+})
+
+///
+
+const luoghi = document.getElementById('luoghi')
+luoghi.addEventListener('mouseover', () => {
+    luoghi.style.color = 'white';
+    luoghi.style.fontSize = '30px';
+
+
+
+})
+luoghi.addEventListener('mouseout', () => {
+    luoghi.style.color = '#E1DA57';
+    luoghi.style.fontSize = '25px';
+
+})
+///
+
+const vers = document.getElementById('vers')
+vers.addEventListener('mouseover', () => {
+    vers.style.color = 'white';
+    vers.style.fontSize = '30px';
+})
+
+vers.addEventListener('mouseout', () => {
+    vers.style.color = '#E1DA57';
+    vers.style.fontSize = '25px';
+})
+
+const qui = document.getElementById('qui')
+qui.addEventListener('mouseover', () => {
+    qui.style.color='white';
+    qui.style.fontSize = '20px';
+})
+
+qui.addEventListener('mouseout', ()=> {
+    qui.style.color='#E1DA57';
+    qui.style.fontSize = '15px';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
